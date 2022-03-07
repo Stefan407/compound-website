d5da4d4b4cbfd1e3fa5fecf4a787c1ea38bfb8da
Stefan Vasyliev <stefanv407@gmail.com>
2022-03-07 15:44:15 +0200

    finish work

--- js/script.js
+++ js/script.js
@@ -46,3 +46,39 @@ tabContent.forEach((item) => {
 item.classList.remove('tab-content_show');
 });
 }
+
+/* Set the width of the side navigation to 0 */
+
+
+let burger = document.querySelector(".container-burger");
+let menu = document.querySelector("#mySidenav");
+let body = document.querySelector("body");
+
+burger.onclick = function(){
+	menu.classList.toggle("active");
+	burger.classList.toggle("change");
+	body.classList.toggle("lock");
+};
+
+function closeNav() {
+	menu.classList.toggle("active");
+	burger.classList.remove("change");
+	body.classList.remove("lock");
+}
+
+
+document.addEventListener('DOMContentLoaded', () => { 
+
+	const button = document.querySelector('.header__button') // находим кнопку для открытия/закрытия окна навигации
+	const nav = document.querySelector('.nav') // находим окно навигации
+ 
+	window.addEventListener('click', e => { // при клике в любом месте окна браузера
+	  const target = e.target // находим элемент, на котором был клик
+	  if (!target.closest('#mySidenav') && !target.closest('.container-burger')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
+		menu.classList.remove('active'); // то закрываем окно навигации, удаляя активный класс
+		burger.classList.remove("change");
+		body.classList.remove("lock");
+	  }
+	})
+ 
+ })
